b96b66b4c90dfff2ecb9f106d1305f48d920dedb
banups <56826632+banups@users.noreply.github.com>
2019-10-23 09:41:22 +0530

    Add files via upload
    Exercise1-Day2- Answers question in exercise1 of day2
    Exercise2-Day2- code to solve Exercise2 of day2

---
+++ Exercise2-Day2.js
@@ -0,0 +1,10 @@
+var words = [ 'Laptop','bus' , 'Parking', 13 ,'453'];
+var arr=[];
+function getLength(){
+for(var i=0;i<words.length;i++){
+arr.push(words[i].length);
+//console.log("array"+arr)
+
+ }}
+ getLength();
+ console.log(arr)
